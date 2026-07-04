edaba502e19bf119a75e3e4bb4d6e270536e1b1a
itsawaisamjad007 <awaisamjad38ala@gmail.com>
2026-07-04 10:35:58 +0500

    Python next learning

--- Dictionry.py
+++ Dictionry.py
@@ -1,4 +1,11 @@
-EMPTY_DICT = {}
-my_message = {"name":"Awais","msg": "hi"}
-print(EMPTY_DICT)
-print(my_message["name"])
+ #Dictionary : It is used to store a key and its value both
+
+EMPTY_DICT = {}                #To create a empty dictionary
+
+my_message = {"name":"Awais","msg": "hi"} # To create a content dictionary
+
+print(EMPTY_DICT)              #To print empty dictionary
+
+print(my_message)              # To print whole dictionary
+
+print(my_message["name"])      # To call particular entity from dictionary
---
+++ set.py
@@ -0,0 +1,20 @@
+
+
+ # Set : Unordered Collection of unique items
+
+# Support union and intersection , Remove duplicate
+
+E_set = set()  #syntax
+                    # to create empty set where we also declare set with empty
+                    # set otherwise it could be considered as a dictionary
+                    # because syntax of dictionary is same as it is
+
+print(type(E_set)) # syntax to show data type of any variable
+
+My_set = {1, 2, 3, 4, 5, 3, 6, 7, 8, 9}  # Syntax
+                    # To create a multiple disordered element set
+
+print(My_set)
+    # Duplicate entity will remove
+     #It is same like a List data can be modified in same format
+     #as in List
