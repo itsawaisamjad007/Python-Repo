466b39e71af8ca6726407f0c7d5f4e60959cb2fd
itsawaisamjad007 <awaisamjad38ala@gmail.com>
2026-07-05 17:57:43 +0500

    initial change

---
+++ Conditiona Statements.py
@@ -0,0 +1,15 @@
+# Condition Statements :-
+                # Conditional statements let program to decide which action to take
+                # based on Conditions.
+#    * if statement
+#    * else statement
+#    * elif statement
+#    * nested statement
+
+a = 5
+b = 10
+if a == b:
+    print("a is equal to b")
+
+else:
+    print("a is not equal to b")
